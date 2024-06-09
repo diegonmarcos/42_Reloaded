a8d55a2af95fd58261bdb2b5042fd8b9c394c7ea
diegonmarcos <diegonmarcos@gmail.com>
2024-06-09 00:33:49 +0000

    19

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnepomuc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/08 04:46:02 by dnepomuc          #+#    #+#             */
+/*   Updated: 2024/06/08 06:20:31 by dnepomuc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main (int argc, char **argv)
+{
+    int i;
+
+    while (i < argc)
+    {
+        ft_putchar(argv[i])
+    }
+    return 0
+}
