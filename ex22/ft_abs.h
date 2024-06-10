773ee6c1eedbe4adc8dd57a2f7fb91266d5adcc4
diegonmarcos <100614489+diegonmarcos@users.noreply.github.com>
2024-06-10 02:18:17 +0200

    Update ft_abs.h

--- ex22/ft_abs.h
+++ ex22/ft_abs.h
@@ -13,12 +13,7 @@
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-#define ABS(Value) ((Value) < 0 ? -(Value) : (Value))
-
-int ft_abs(int value)
-{
-    return ABS(value);
-}
+# define ABS(x) x > 0 ? x : -x
 
 #endif
 
